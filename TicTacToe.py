f678cb038d709e28ba669442c98f218a09af94d4
Mason Scarborough <masonwhispi@gmail.com>
2024-12-11 16:56:41 +0000

    win condition stuff

--- TicTacToe.py
+++ TicTacToe.py
@@ -7,3 +7,13 @@ def print_board(board):
     print(f"{board[6]} | {board[7]} | {board[8]}")
     print("\n")
 
+def check_win(board, player):
+    win_conditions = [
+        [0, 1, 2], [3, 4, 5], [6, 7, 8],
+        [0, 3, 6], [1, 4, 7], [2, 5, 8],
+        [0, 4, 8], [2, 4, 6]
+    ]
+    for condition in win_conditions:
+        if board[condition[0]] == board[condition[1]] == board[condition[2]] == player:
+            return True
+    return False
