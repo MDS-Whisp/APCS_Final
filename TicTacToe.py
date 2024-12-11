c9a42f4560b8d06d2cc1230bc3bc42fe131e3b33
Mason Scarborough <masonwhispi@gmail.com>
2024-12-11 18:54:53 +0000

    more code

--- TicTacToe.py
+++ TicTacToe.py
@@ -17,3 +17,36 @@ def check_win(board, player):
         if board[condition[0]] == board[condition[1]] == board[condition[2]] == player:
             return True
     return False
+
+def check_draw(board):
+    return ' ' not in board
+
+def tic_tac_toe():
+    board = [' ' for _ in range(9)]
+    current_player = 'X'
+    while True:
+        print_board(board)
+        
+        move = -1
+        while move < 0 or move > 8 or board[move] != ' ':
+            try:
+                move = int(input(f"Player {current_player}, enter a position (1-9): ")) - 1
+            except ValueError:
+                continue
+        
+        board[move] = current_player
+        
+        if check_win(board, current_player):
+            print_board(board)
+            print(f"Player {current_player} wins!")
+            break
+        
+        if check_draw(board):
+            print_board(board)
+            print("It's a draw!")
+            break
+        
+        current_player = 'O' if current_player == 'X' else 'X'
+
+if __name__ == "__main__":
+    tic_tac_toe()
