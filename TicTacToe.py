0f19b347e0fe3f0a0d6260520f425b6e3781bf3f
Mason Scarborough <masonwhispi@gmail.com>
2024-12-11 16:45:28 +0000

    added code

--- TicTacToe.py
+++ TicTacToe.py
@@ -1,1 +1,52 @@
-blest bext
+def print_board(board):
+    print("\n")
+    print(f"{board[0]} | {board[1]} | {board[2]}")
+    print("--+---+--")
+    print(f"{board[3]} | {board[4]} | {board[5]}")
+    print("--+---+--")
+    print(f"{board[6]} | {board[7]} | {board[8]}")
+    print("\n")
+
+def check_win(board, player):
+    win_conditions = [
+        [0, 1, 2], [3, 4, 5], [6, 7, 8],
+        [0, 3, 6], [1, 4, 7], [2, 5, 8],
+        [0, 4, 8], [2, 4, 6]
+    ]
+    for condition in win_conditions:
+        if board[condition[0]] == board[condition[1]] == board[condition[2]] == player:
+            return True
+    return False
+
+def check_draw(board):
+    return ' ' not in board
+
+def tic_tac_toe():
+    board = [' ' for _ in range(9)]
+    current_player = 'X'
+    while True:
+        print_board(board)
+        
+        move = -1
+        while move < 0 or move > 8 or board[move] != ' ':
+            try:
+                move = int(input(f"Player {current_player}, enter a position (1-9): ")) - 1
+            except ValueError:
+                continue
+        
+        board[move] = current_player
+        
+        if check_win(board, current_player):
+            print_board(board)
+            print(f"Player {current_player} wins!")
+            break
+        
+        if check_draw(board):
+            print_board(board)
+            print("It's a draw!")
+            break
+        
+        current_player = 'O' if current_player == 'X' else 'X'
+
+if __name__ == "__main__":
+    tic_tac_toe()
